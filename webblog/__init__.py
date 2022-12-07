afebac31851f21a40a5a524f33eea63dd528ad7b
Nhat Pham <99010618+nhatpham001@users.noreply.github.com>
2022-12-07 03:42:48 -0500

    Update __init__.py

--- webblog/__init__.py
+++ webblog/__init__.py
@@ -1,10 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Mysecretkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from webblog import routes

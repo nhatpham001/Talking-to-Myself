5e77cd0804d500811c79d976459d91e1b70502f7
Nhat Pham <99010618+nhatpham001@users.noreply.github.com>
2022-12-25 16:41:14 -0500

    Update forms.py

--- webblog/forms.py
+++ webblog/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from webblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min =2, max = 20)])
@@ -9,8 +11,34 @@ class RegistrationForm(FlaskForm):
     confirm_password  = PasswordField('confirm password', validators= [ DataRequired(), EqualTo('password')])
     submit = SubmitField('sign up')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError(' username taken!')
+
+    def validate_email(self, email):
+        email = User.query.filter_by(email = email.data).first()
+        if email:
+            raise ValidationError(' email has been used!')
+
 class LoginForm(FlaskForm):
     email    = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('remeber me')
     submit = SubmitField('log in')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min =2, max = 20)])
+    email    = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username = username.data).first()
+            if user:
+                raise ValidationError(' username taken!')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            email = User.query.filter_by(email = email.data).first()
+            if email:
+                raise ValidationError(' email has been used!')
